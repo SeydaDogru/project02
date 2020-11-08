aa2f6db7e0de6f88b88f6fece87247f46ded6b94
SeydaDogru <34250257+SeydaDogru@users.noreply.github.com>
2020-11-08 11:12:20 -0600

    forth commit from remote master branch

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -14,6 +14,8 @@ public class FirstClass {
 	System.out.println("second commit form remote master branch");
 		
 	System.out.println("third commit form remote master branch");
+		
+	System.out.println("Here is a merge conflict we have from master branch");
 
 	}
 
